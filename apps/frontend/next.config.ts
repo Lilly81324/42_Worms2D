7bc6367b951de241bf949d54195d3ff1a800d98d
Thang Bui Quoc <tbuiloves42@gmail.com>
2026-05-20 19:39:03 +0200

    - removed dev indicator because it does nothing in dev there is still the indicator and now its strict typesafe

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
- 
-const nextConfig = {
-    // Make the little Next Logo from the bottom left only appear in development mode
-    devIndicators: (process.env.NODE_ENV == "development"),
+
+const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com', // Google's image server for profile pictures
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
             },
         ],
     },
